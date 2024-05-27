c95125025100441368231f9a1f1c2bdb7b98f1e8
Chelokek10 <153737506+Chelokek10@users.noreply.github.com>
2024-05-27 10:58:30 +0300

    styles.js

--- src/styles.js
+++ src/styles.js
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 import { animated } from 'react-spring';
 
 export const ResumeContainer = styled.div` // Стиль контейнера для резюме
-  max-width: 800px;  // Максимальна ширина контейнера
-  margin: 0 auto;// Автоматичне вирівнювання контейнера по центру відносно батьківського елемента
-  padding: 20px; // Відступи всередині контейнера
-  border: 1px solid #ddd; // Рамка з тінню
-  border-radius: 10px; // Радіус закруглення кутів контейнера
-  background-color: #f9f9f9;// Колір фону контейнера
+  max-width: 800px;  // ширина контейнера
+  margin: 0 auto;
+  padding: 20px; 
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  background-color: #f9f9f9;
 `;
 
 export const Title = styled.h1` // Стиль заголовока першого рівня 
-  text-align: center; // Вирівнювання тексту по центру
+  text-align: center; 
   font-size: 2em; // Розмір шрифту
 `;
 
@@ -21,16 +21,16 @@ export const Section = styled.div`
 
 export const SectionTitle = styled.h2`
   font-size: 1.5em; // Розмір шрифту заголовків 2 рівня
-  color: #333; // Колір тексту
+  color: #333; 
 `;
 
 export const Field = styled.div`
   margin-bottom: 10px; // Відступ між полями
-  flex-direction: column; // Напрямок колонок
+  flex-direction: column; 
 `;
 
 export const Label = styled.label`
-  display: block; // Блоковий елемент
+  display: block; 
   font-weight: bold; // Товщина шрифту
   margin-bottom: 5px; // Відступ поля від назви поля
 `;
@@ -56,18 +56,18 @@ export const AnimatedTextAreaField = styled(animated.textarea)`
 
 export const InputField = styled.input`
   width: 100%; // Ширина поля окім полів Опис
-  padding: 10px; // Висота поля
-  font-size: 1em; // Розмір шрифту
-  border: 1px solid #ccc; // Рамка з тонкою сірою лінією
-  border-radius: 50px; // Радіус закруглення кутів
-  margin-bottom: 10px; // Відступ між полями
+  padding: 10px; 
+  font-size: 1em; 
+  border: 1px solid #ccc; 
+  border-radius: 50px; 
+  margin-bottom: 10px; 
 `;
 
 export const TextAreaField = styled.textarea`
   width: 100%; // Ширина поля
-  padding: 10px; // Внутрішні відступи
-  font-size: 1em;// Розмір шрифту
-  border: 1px solid #ccc; // Рамка з тонкою сірою лінією
-  border-radius: 50px; // Радіус закруглення кутів
-  margin-bottom: 10px; // Відступ знизу Описів
+  padding: 10px; 
+  font-size: 1em;
+  border: 1px solid #ccc; 
+  border-radius: 50px; 
+  margin-bottom: 10px; 
 `;
